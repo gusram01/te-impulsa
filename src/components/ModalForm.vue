2b029ce061533d7f9c4077d8b3eedef75aa0dedf
Gus Ramírez <g.ram.bt@hotmail.com>
2021-04-16 22:52:17 -0500

    newService styles ready; bug with post service

--- src/components/ModalForm.vue
+++ src/components/ModalForm.vue
@@ -2,37 +2,49 @@
   <ion-header translucent>
     <ion-toolbar>
       <ion-title>{{ title }}</ion-title>
-      <ion-buttons slot="end">
+      <!-- <ion-buttons slot="end">
         <ion-button @click="closeModal">
           <ion-icon :icon="closeOutline"></ion-icon>
           close
         </ion-button>
-      </ion-buttons>
+      </ion-buttons> -->
     </ion-toolbar>
   </ion-header>
   <ion-content class="ion-padding">
     <ion-list lines="none">
       <ion-item>
-        <ion-label>Full name</ion-label>
+        <ion-label>First name</ion-label>
         <ion-input
           type="text"
-          placeholder="Full name"
+          placeholder="First Name"
           clear-input
-          @IonInput="fullName = $event.target.value"
+          @IonInput="name = $event.target.value"
+        ></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>Last Name</ion-label>
+        <ion-input
+          type="text"
+          placeholder="Last Name"
+          clear-input
+          @IonInput="lastName = $event.target.value"
         ></ion-input>
       </ion-item>
       <ion-item>
         <ion-label>Telephone</ion-label>
         <ion-input
-          type="text"
+          type="number"
           placeholder="Telephone"
           clear-input
-          @IonInput="telephone = $event.target.value"
+          @IonInput="phone = $event.target.value"
         ></ion-input>
       </ion-item>
       <ion-item>
         <ion-label>Date</ion-label>
-        <ion-datetime display-format="MM/DD/YYYY"></ion-datetime>
+        <ion-datetime
+          display-format="MM/DD/YYYY"
+          @IonChange="date_service = $event.target.value"
+        ></ion-datetime>
       </ion-item>
       <ion-item>
         <ion-label>Direction</ion-label>
@@ -40,13 +52,13 @@
           type="text"
           placeholder="Direction"
           clear-input
-          @IonInput="direction = $event.target.value"
+          @IonInput="address = $event.target.value"
         ></ion-input>
       </ion-item>
       <ion-item>
         <ion-label>Department</ion-label>
         <ion-input
-          type="text"
+          type="number"
           placeholder="Department"
           clear-input
           @IonInput="department = $event.target.value"
@@ -55,40 +67,51 @@
       <ion-item>
         <ion-label>Comuna</ion-label>
         <ion-input
-          type="text"
+          type="number"
           placeholder="Comuna"
           clear-input
           @IonInput="comuna = $event.target.value"
         ></ion-input>
       </ion-item>
 
-      <ion-button type="cancel" @click="closeModal" color="light">
-        Cancel
-      </ion-button>
-      <ion-button type="submit" @click="submitForm" color="tertiary">
-        Save
-      </ion-button>
+      <ion-grid>
+        <ion-row>
+          <ion-col> </ion-col>
+          <ion-col>
+            <ion-button type="cancel" @click="closeModal" color="light">
+              Cancel
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button type="submit" @click="openToast" color="tertiary">
+              Save
+            </ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-list>
   </ion-content>
 </template>
 
 <script>
 import {
-  // IonPage,
+  IonGrid,
+  IonCol,
+  IonRow,
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
-  IonButtons,
   IonButton,
-  IonIcon,
   IonInput,
   IonLabel,
   IonItem,
   IonList,
   IonDatetime,
+  toastController,
 } from '@ionic/vue';
 import { closeOutline } from 'ionicons/icons';
+import { newService } from '../services/api';
 
 export default {
   name: 'Modal',
@@ -96,40 +119,148 @@ export default {
     title: { type: String, default: 'Super Modal' },
     close: { type: Function },
   },
+
+  mounted() {
+    if (!this.name.length) {
+      this.errorInput.name = 'name';
+    }
+
+    if (!this.lastName.length) {
+      this.errorInput.lastName = 'lastName';
+    }
+
+    if (this.phone <= 0 || isNaN(+this.phone)) {
+      this.errorInput.phone = 'phone';
+    }
+
+    if (!this.date_service?.length) {
+      this.errorInput.date_service = 'date_service';
+    }
+
+    if (!this.address.length) {
+      this.errorInput.address = 'address';
+    }
+
+    if (this.department <= 0 || isNaN(+this.department)) {
+      this.errorInput.department = 'department';
+    }
+
+    if (this.comuna <= 0 || isNaN(+this.comuna)) {
+      this.errorInput.comuna = 'comuna';
+    }
+  },
+
   data() {
     return {
       closeOutline,
+      toast: null,
       content: 'Content',
-      fullName: '',
-      telephone: '',
-      date: null,
-      direction: '',
+      name: '',
+      lastName: '',
+      phone: '',
+      date_service: null,
+      address: '',
       department: '',
       comuna: '',
+      errorInput: {},
     };
   },
+
+  watch: {
+    name(val) {
+      if (!val) {
+        this.errorInput.name = 'name';
+      } else {
+        delete this.errorInput.name;
+      }
+    },
+    lastName(val) {
+      if (!val) {
+        this.errorInput.lastName = 'lastName';
+      } else {
+        delete this.errorInput.lastName;
+      }
+    },
+    phone(val) {
+      if (!val || isNaN(+val)) {
+        this.errorInput.phone = 'phone';
+      } else {
+        delete this.errorInput.phone;
+      }
+    },
+    date_service(val) {
+      if (!val) {
+        this.errorInput.date_service = 'date_service';
+      } else {
+        delete this.errorInput.date_service;
+      }
+    },
+    address(val) {
+      if (!val) {
+        this.errorInput.address = 'address';
+      } else {
+        delete this.errorInput.address;
+      }
+    },
+    department(val) {
+      if (!val || isNaN(+val)) {
+        this.errorInput.department = 'department';
+      } else {
+        delete this.errorInput.department;
+      }
+    },
+    comuna(val) {
+      if (!val || isNaN(+val)) {
+        this.errorInput.comuna = 'comuna';
+      } else {
+        delete this.errorInput.comuna;
+      }
+    },
+  },
+
   methods: {
     closeModal() {
       this.close();
     },
-    submitForm() {
-      console.log('submit');
+
+    async openToast() {
+      if (!Object.keys(this.errorInput).length) {
+        this.submitForm();
+        return;
+      }
+      this.toast = await toastController.create({
+        message: `Please verify ${Object.values(this.errorInput).join(', ')}`,
+        position: 'bottom',
+        duration: 2000,
+        color: 'warning',
+      });
+      this.toast.present();
     },
-  },
-  watch: {
-    fullName() {
-      console.log(this.fullName);
+
+    submitForm() {
+      const service = {
+        name: this.name,
+        lastName: this.lastName,
+        phone: this.phone,
+        date_service: this.date_service,
+        address: this.address,
+        department: this.department,
+        comuna: this.comuna,
+        token_business: '841f80d3-7f4c-11eb-b629-f603cfed5859',
+      };
+
+      newService(service);
     },
   },
   components: {
-    // IonPage,
+    IonGrid,
+    IonCol,
+    IonRow,
     IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
-    IonButtons,
     IonButton,
-    IonIcon,
     IonInput,
     IonLabel,
     IonItem,
@@ -149,8 +280,9 @@ ion-datetime {
   --padding-bottom: 0.8em;
   --padding-start: 1.5em;
   --padding-end: 1.5em;
+  width: 100%;
   margin: 0.5em;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.35);
   border-radius: 10px;
 }
 </style>
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import qs from 'qs';
 
 const url = process.env.VUE_APP_URL;
 const token_business = process.env.VUE_APP_TOKEN;
@@ -10,6 +11,24 @@ export const getInfo = () =>
       if (!resp.data.success) {
         throw new Error('Empty data');
       }
+      return resp.data.data;
+    })
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
+
+export const newService = (service) =>
+  axios
+    .post(`${url}/newService`, {
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(service),
+    })
+    .then((resp) => {
+      if (!resp.data.success) {
+        throw new Error('Empty data');
+      }
+      console.log(resp);
       return resp.data.data;
     })
     .catch((err) => {
